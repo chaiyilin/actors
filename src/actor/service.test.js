ff417a64900a41dc38dcf70bce118a7367be21ab
yilin chai <chaiyilin@hotmail.com>
2017-11-12 22:28:47 +1100

    lint

--- src/actor/service.test.js
+++ src/actor/service.test.js
@@ -1,23 +1,25 @@
-import fetchMovies from './service';
-import movies from './service.test.json';
-import transform from './utils';
+import fetchMovies from './service'
+import movies from './service.test.json'
+import transform from './utils'
 
+// const movies = require('./service.test.json')
 // https://github.com/facebook/jest/issues/238
 // https://stackoverflow.com/questions/36571357/difference-between-unmock-and-dontmock-in-jest
 // http://facebook.github.io/jest/docs/en/manual-mocks.html, last section
-jest.unmock('./service.test.json');
+// jest.unmock('./service.test.json')
 
 describe('service', () => {
   beforeEach(() => {
-    fetch.mockReturnValue(movies);
-  });
+    fetch.mockReturnValue(movies)
+  })
 
   it('should fetch and transformed', async () => {
-    const data = await fetchMovies();
-    expect(transform).toBeCalledWith(data);
-  });
+    console.log(movies)
+    const data = await fetchMovies()
+    expect(transform).toBeCalledWith(data)
+  })
 
   afterEach(() => {
-    fetch.resetMocks();
-  });
-});
+    fetch.resetMocks()
+  })
+})
